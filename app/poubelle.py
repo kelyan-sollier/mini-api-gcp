293d3ab6fc3663ee4cbe6435c530b8a3cfc596a7
Pierre Astor <166384908+Mifaxo@users.noreply.github.com>
2025-05-20 17:16:45 +0200

    Update poubelle.py

--- app/poubelle.py
+++ app/poubelle.py
@@ -2,7 +2,7 @@ from vertexai.preview.generative_models import GenerativeModel
 import vertexai
 
 vertexai.init(project="mini-projet-data-tools", location="us-central1")
-
+response = model.generate_content("Raconte-moi une blague.")
 model = GenerativeModel("gemini-1.5-flash")  # ou gemini-1.5-pro
 
 print(response.text)
